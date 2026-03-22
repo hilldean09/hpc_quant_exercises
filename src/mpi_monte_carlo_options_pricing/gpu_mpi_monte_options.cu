08c34b3a39148e3ed81beadfc03c69c27035d880
Dean Hill <inkandflowers@proton.me>
2026-03-22 09:24:21 +0800

    Write GPU algorith memory allocation structure

--- src/mpi_monte_carlo_options_pricing/gpu_mpi_monte_options.cu
+++ src/mpi_monte_carlo_options_pricing/gpu_mpi_monte_options.cu
@@ -3,6 +3,7 @@
 #include "./gpu_mpi_monte_options.hpp"
 
 #include <cstdlib>
+#include <cuda.h>
 
 
 #include "./mpi_monte_options.hpp"
@@ -23,12 +24,34 @@ float Run_GPU_Accelerated_Simulation( unsigned long long total_runs,
   float* host_random_number_array;
   float* host_call_price_array;
 
-  host_random_number_array = ( float* ) std::malloc( total_timesteps * total_runs * sizeof( float ) );
-  host_call_price_array = ( float* ) std::malloc( total_timesteps * total_runs * sizeof( float ) );
+  float* device_random_number_array_1;
+  float* device_random_number_array_2;
+  float* device_variance_array;
+  float* device_call_price_array_1;
+  float* device_call_price_array_2;
 
-  
+  // Allocating host memory
+  host_random_number_array = ( float* ) std::malloc( 2 * total_timesteps * concurrent_runs * sizeof( float ) );
+  host_call_price_array = ( float* ) std::malloc( total_timesteps * concurrent_runs * sizeof( float ) );
+
+
+  // Allocating device memory
+  cudaMalloc( &device_random_number_array_1, 2 * total_timesteps * concurrent_runs * sizeof( float ) );
+  cudaMalloc( &device_random_number_array_2, 2 * total_timesteps * concurrent_runs * sizeof( float ) );
+  cudaMalloc( &device_variance_array, total_timesteps * concurrent_runs * sizeof( float ) );
+  cudaMalloc( &device_call_price_array_1, total_timesteps * concurrent_runs * sizeof( float ) );
+  cudaMalloc( &device_call_price_array_2, total_timesteps * concurrent_runs * sizeof( float ) );
+
+
+  // Freeing Memory
+  cudaFree( device_random_number_array_1 );
+  cudaFree( device_random_number_array_2 );
+  cudaFree( device_variance_array );
+  cudaFree( device_call_price_array_1 );
+  cudaFree( device_call_price_array_2 );
 
   std::free( host_random_number_array );
+  std::free( host_call_price_array );
 }
 
 
