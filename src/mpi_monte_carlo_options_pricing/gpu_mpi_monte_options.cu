a8b48cb394aaf587acd54d63ef2e7d9e61d89dc6
Dean Hill <inkandflowers@proton.me>
2026-03-22 09:11:33 +0800

    Outline structure of GPU algorithm interfaces

--- src/mpi_monte_carlo_options_pricing/gpu_mpi_monte_options.cu
+++ src/mpi_monte_carlo_options_pricing/gpu_mpi_monte_options.cu
@@ -1,1 +1,34 @@
+
 // GPU Accelerated Algorithm
+#include "./gpu_mpi_monte_options.hpp"
+
+#include <cstdlib>
+
+
+#include "./mpi_monte_options.hpp"
+
+float Run_GPU_Accelerated_Simulation( unsigned long long total_runs,
+                                      unsigned long long total_timesteps,
+                                      unsigned long long seed,
+                                      bool do_write_to_file,
+                                      Heston_Parameters parameters,
+                                      float strike_price,
+                                      float discounting_rate,
+                                      unsigned long long concurrent_runs,
+                                      bool do_collect_statistics,
+                                      Simulation_Statistics simulation_stats ) {
+
+  float output_call_price = 0.0;
+
+  float* host_random_number_array;
+  float* host_call_price_array;
+
+  host_random_number_array = ( float* ) std::malloc( total_timesteps * total_runs * sizeof( float ) );
+  host_call_price_array = ( float* ) std::malloc( total_timesteps * total_runs * sizeof( float ) );
+
+  
+
+  std::free( host_random_number_array );
+}
+
+
--- src/mpi_monte_carlo_options_pricing/gpu_mpi_monte_options.hpp
+++ src/mpi_monte_carlo_options_pricing/gpu_mpi_monte_options.hpp
@@ -2,10 +2,19 @@
 #ifndef MMCOP_GPU_MPI_MONTE_OPTIONS_HPP
 #define MMCOP_GPU_MPI_MONTE_OPTIONS_HPP
 
+#include "./io_mpi_monte_options.hpp"
+#include "./mpi_monte_options.hpp"
+#include "./pre_controls.hpp"
+
 float Run_GPU_Accelerated_Simulation( unsigned long long total_runs,
                                       unsigned long long total_timesteps,
                                       unsigned long long seed,
                                       bool do_write_to_file,
                                       Heston_Parameters parameters,
                                       float strike_price,
-                                      float discounting_rate );
+                                      float discounting_rate,
+                                      unsigned long long concurrent_runs = MMCOP_GPU_DEFAULT_CONCURRENT_RUNS,
+                                      bool do_collect_statistics = false,
+                                      Simulation_Statistics simulation_stats = nullptr );
+
+#endif
